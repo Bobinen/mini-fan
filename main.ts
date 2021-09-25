ed7bc5e3dd128d204a8b2b98ab082b10d2830024
Bobinen <91364071+Bobinen@users.noreply.github.com>
2021-09-25 15:47:52 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P1) >= 100) {
+    if (pins.analogReadPin(AnalogPin.P0) >= 100) {
         basic.showIcon(IconNames.Heart)
         pins.digitalWritePin(DigitalPin.P0, 1)
-        basic.showIcon(IconNames.SmallHeart)
+    } else {
         pins.digitalWritePin(DigitalPin.P0, 0)
     }
 })
